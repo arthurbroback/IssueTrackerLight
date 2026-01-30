5ae57709709df87dd23c287b3b417a0ff8ec59be
arthurbroback <arthur.broback@gmail.com>
2026-01-30 13:31:24 +0100

    Add create issue form with validation

---
+++ IssueTrackerLight/Models/IssueCreateModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTrackerLight.Models;
+
+public sealed class IssueCreateModel
+{
+    [Required(ErrorMessage = "Titel krävs.")]
+    [StringLength(60)]
+    public string Title { get; set; } = "";
+
+    [Required(ErrorMessage = "Beskrivning krävs.")]
+    [StringLength(500)]
+    public string Description { get; set; } = "";
+}
